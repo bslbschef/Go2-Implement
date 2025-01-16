2e01945815368164f56cb1634f3189ea6fcd66de
bslbschef <bslbschef@gmail.com>
2025-01-16 16:18:13 +0800

    train.py modification

---
+++ go2_train.py
@@ -0,0 +1,10 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--exp_name", type=str, default="go2-walking")
+    
+
+if __name__ == "__main__":
+    main()
